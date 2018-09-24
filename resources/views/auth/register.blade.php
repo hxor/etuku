f66345d7caf99af1e0b3b91fed0db014c006698b
idstck <idstackdeveloper@gmail.com>
2018-09-24 15:04:14 +0700

    rename app

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 <div class="wrapper-page">
     <div class=" card-box">
         <div class="panel-heading">
-            <h3 class="text-center"> Sign In to <strong class="text-custom">e-Komoditas</strong> </h3>
+            <h3 class="text-center"> Sign In to <strong class="text-custom">e-TUKU</strong> </h3>
         </div>
         <div class="panel-body">
             <form class="form-horizontal" method="POST" action="{{ route('register') }}">
